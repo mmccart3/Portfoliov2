27587927c40298929f66c0b3a826f53abf1419c1
mmccart3 <mark.joseph.mccarthy@gmail.com>
2021-11-13 07:49:08 +0000

    firstcommit

---
+++ scripts/slideshow.js
@@ -0,0 +1,25 @@
+slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  slides = document.getElementsByClassName("mySlides");
+  dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+function currentSlide(n) {
+  showSlides(slideIndex = n-1);
+}
